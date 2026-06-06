36a55d0fffc0b1d0d9b1275f3de413ee1f2d95be
Axel Montiel <axel.avi.fer@gmail.com>
2026-06-06 10:30:52 -0600

    feat: add ejercicio2

---
+++ EjercicioDos/src/dgtic/modelo/ExplicarClase.java
@@ -0,0 +1,9 @@
+package dgtic.modelo;
+
+public class ExplicarClase implements Responsabilidades{
+
+    @Override
+    public void realizar() {
+        System.out.println("Explicar la clase");
+    }
+}
